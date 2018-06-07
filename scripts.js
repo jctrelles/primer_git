c72156fa82b249d552f5b7ed49a3c3698dbc695d
jctrelles <trelles.jc@gmail.com>
2018-06-06 20:38:36 -0700

    agregue un chiste

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 Aqui va mas codigo
+
+y ahora un chiste: 
+Esto es un ladrón recién salido de los juzgados y le llama su mujer:
+- Pepe, ¿qué te han dicho? 
+
+- Pues me han dicho que  3 años de cárcel o 100.000 euros. 
+
+- No seas tonto Pepe, ¡¡coge el dinero!!
